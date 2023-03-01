e939910b65167d828b9281f8261f09bc23edf3fb
Etienne <Etienne.ress@gmail.com>
2023-03-01 17:06:02 +0100

    Add project files.

---
+++ Bisection/Program.cs
@@ -0,0 +1,29 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+    Start:
+        Console.WriteLine("\n--Neue Rechnung--\n");
+
+        double n;
+    InputNumber:
+        Console.Write("Wurzel aus: ");
+        try { n = Convert.ToDouble(Console.ReadLine()); } catch { Console.WriteLine("Ungültige Eingabe!"); goto InputNumber; }
+        double low = 0, high = n, middle = 0, lastm = 1;
+
+        Console.WriteLine("Berechne Wurzel aus: " + n + "...");
+        while (middle * middle != n && lastm != middle)
+        {
+            lastm = middle;
+            middle = (low + high) / 2;
+            if (middle * middle > n)
+            {
+                high = middle;
+            }
+            else low = middle;
+            Console.WriteLine(middle);
+        }
+        Console.WriteLine("Wurzel: " + middle);
+        goto Start;
+    }
+}
