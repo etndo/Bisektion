c79f20fc7eacdd12f1e840a6e221b05c5527b0f7
Etienne <Etienne.ress@gmail.com>
2023-03-01 20:15:28 +0100

    Added Visualisation

--- Bisection/Program.cs
+++ Bisection/Program.cs
@@ -1,4 +1,5 @@
-﻿internal class Program
+﻿using static Bisection.Visualisation;
+internal class Program
 {
     private static void Main(string[] args)
     {
@@ -6,22 +7,24 @@
         Console.WriteLine("\n--Neue Rechnung--\n");
 
         double n;
-    InputNumber:
+    InputNumber: //Gets the number to calculate the square root of
         Console.Write("Wurzel aus: ");
         try { n = Convert.ToDouble(Console.ReadLine()); } catch { Console.WriteLine("Ungültige Eingabe!"); goto InputNumber; }
         double low = 0, high = n, middle = 0, lastm = 1;
 
         Console.WriteLine("Berechne Wurzel aus: " + n + "...");
-        while (middle * middle != n && lastm != middle)
+        setStartVal(high);
+        drawBars(low, high); //Visualizes initial input
+        while (middle * middle != n /*Dont go on if solved*/ && lastm != middle /*Dont loop forever*/)
         {
             lastm = middle;
             middle = (low + high) / 2;
             if (middle * middle > n)
             {
-                high = middle;
+                high = middle; //Sets the new cap
             }
-            else low = middle;
-            Console.WriteLine(middle);
+            else low = middle; //Sets the new floor
+            drawBars(low, high); //Visualizes
         }
         Console.WriteLine("Wurzel: " + middle);
         goto Start;
---
+++ Bisection/Visualisation.cs
@@ -0,0 +1,61 @@
+﻿namespace Bisection
+{
+    internal class Visualisation
+    {
+        static double startHigh;
+        public static void setStartVal(double high)
+        {
+            startHigh = high; //Value to scale the length of both bars to
+        }
+
+        public static void drawBars(double low, double high)
+        {
+            double highLength = high / startHigh * 30; //Sets length of High bar
+            double lowLength = low / startHigh * 30; //^ Low bar (*30 cause of the bars length of 30)
+            string lowString = low + "";
+            int buffer = 19 - lowString.Length; //Buffer so its all alligned, 19 == max double string length
+            string lowBar = "";
+            string highBar = "";
+
+            //Bar for the lower val
+            for (int i = 0; i < buffer; i++) //Writes buffer
+            {
+                Console.Write(" ");
+            }
+            Console.Write(low + " ");
+
+            for (int i = 0; i < lowLength; i++)
+            {
+                lowBar = lowBar + "█";
+            }
+            for (int i = lowBar.Length; i < 30; i++) //Needed amount of empty rectangles for length of 30
+            {
+                lowBar = lowBar + "▓";
+            }
+            lowBar = Reverse(lowBar); //Reverses it so the filled part extends from the middle
+            Console.Write(lowBar);
+
+            Console.Write("|");
+
+            //Bar for the higher val
+            for (int i = 0; i < highLength; i++)
+            {
+                highBar = highBar + "█";
+            }
+            for (int i = highBar.Length; i < 30; i++) //Needed amount of empty rectangles for length of 30
+            {
+                highBar = highBar + "░";
+            }
+            Console.Write(highBar);
+
+            Console.Write( " " + high + "\n");
+        }
+
+        static string Reverse(string s)
+        {
+            char[] charArray = s.ToCharArray();
+            Array.Reverse(charArray);
+            return new string(charArray);
+        }
+    }
+}
